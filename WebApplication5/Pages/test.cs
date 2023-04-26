eba63db1dd42fdc2004b35ba6a969cb606897fc4
Vedika Khanna <vedika.khanna@nagarro.com>
2023-04-26 12:45:19 +0530

    second commit

---
+++ WebApplication5/Pages/test.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication5.Pages
+{
+    public class test
+    {
+
+        string var = "new";
+
+        static void Main(string[] args)
+        {
+            test myObj = new test();
+            Console.WriteLine(myObj.var);
+        }
+    }
+}
